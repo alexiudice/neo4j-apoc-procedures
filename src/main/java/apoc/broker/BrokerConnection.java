c2ccc915627124c8781a25ed963395dc9398cfe1
Alex Iudice <alex.iudice@atomrain.com>
2018-08-28 14:10:11 -0400

    fixes #905 - Added generic broker integration with configurable connections by namespace. Added integration for RabbitMQ, SQS, and Kafka.

---
+++ src/main/java/apoc/broker/BrokerConnection.java
@@ -0,0 +1,18 @@
+package apoc.broker;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.neo4j.procedure.Name;
+
+public interface BrokerConnection
+{
+    String maxPollRecordsDefault = "1";
+
+    Stream<BrokerMessage> send( @Name( "message" ) Map<String,Object> message, @Name( "configuration" ) Map<String,Object> configuration );
+
+    Stream<BrokerResult> receive( @Name( "configuration" ) Map<String,Object> configuration ) throws IOException;
+
+    void stop();
+}
